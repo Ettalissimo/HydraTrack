0c0ae0103cdd8b5dd0ec29ece29b79d5297c5524
Ettalissimo <oussamaettaouil13@gmail.com>
2024-05-21 13:58:41 +0100

    Loading page

--- app/src/main/java/com/example/water_app/SplashScreenActivity.java
+++ app/src/main/java/com/example/water_app/SplashScreenActivity.java
@@ -17,7 +17,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         Runnable runnable = new Runnable(){
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), Login.class);
                 startActivity(intent);
                 finish();
             }
